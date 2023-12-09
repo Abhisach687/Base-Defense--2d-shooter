c5a4a1bf17d166ad1cec7aa2097d9943667a0728
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2023-12-09 12:11:19 +0545

    homing spinning enemies

--- index.js
+++ index.js
@@ -98,9 +98,22 @@ class Enemy {
     document.body.appendChild(this.element);
 
     this.type = "Linear";
+    this.radians = 0;
+    this.center = {
+      x,
+      y,
+    };
 
     if (Math.random() < 0.5) {
       this.type = "Homing";
+
+      if (Math.random() < 0.5) {
+        this.type = "Spinning";
+
+        if (Math.random() < 0.5) {
+          this.type = "Homing Spinning";
+        }
+      }
     }
   }
 
@@ -116,14 +129,37 @@ class Enemy {
   update() {
     this.draw();
 
-    if (this.type === "Homing") {
-      const angle = Math.atan2(player.y - this.y, player.x - this.x);
+    if (this.type === "Spinning") {
+      this.radians += 0.1;
+
+      this.center.x += this.velocity.x;
+      this.center.y += this.velocity.y;
+
+      this.x = this.center.x + Math.cos(this.radians) * 30;
+      this.y = this.center.y + Math.sin(this.radians) * 30;
+    } else if (this.type === "Homing") {
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+    } else if (this.type === "Homing Spinning") {
+      this.radians += 0.1;
+
+      const angle = Math.atan2(
+        player.y - this.center.y,
+        player.x - this.center.x
+      );
       this.velocity.x = Math.cos(angle);
       this.velocity.y = Math.sin(angle);
-    }
 
-    this.x = this.x + this.velocity.x;
-    this.y = this.y + this.velocity.y;
+      this.center.x += this.velocity.x;
+      this.center.y += this.velocity.y;
+
+      this.x = this.center.x + Math.cos(this.radians) * 30;
+      this.y = this.center.y + Math.sin(this.radians) * 30;
+    } else {
+      // linear movement
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+    }
   }
 }
 
