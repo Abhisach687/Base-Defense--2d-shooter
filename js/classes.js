033a6204756c0bf5f6927817732835bf3d05ca86
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2023-12-09 13:14:30 +0545

    refactor

---
+++ js/classes.js
@@ -0,0 +1,182 @@
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+
+    const friction = 0.99;
+
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+
+    // collision detection for x axis
+    if (
+      this.x + this.radius + this.velocity.x <= canvas.width &&
+      this.x - this.radius + this.velocity.x >= 0
+    ) {
+      this.x += this.velocity.x;
+    } else {
+      this.velocity.x = 0;
+    }
+
+    // collision detection for y axis
+    if (
+      this.y + this.radius + this.velocity.y <= canvas.height &&
+      this.y - this.radius + this.velocity.y >= 0
+    ) {
+      this.y += this.velocity.y;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    //for smooth resizing of big enemy upon hit
+    this.element = document.createElement("div");
+    this.element.style.width = `${2 * radius}px`;
+    this.element.style.height = `${2 * radius}px`;
+    document.body.appendChild(this.element);
+
+    this.type = "Linear";
+    this.radians = 0;
+    this.center = {
+      x,
+      y,
+    };
+
+    if (Math.random() < 0.5) {
+      this.type = "Homing";
+
+      if (Math.random() < 0.5) {
+        this.type = "Spinning";
+
+        if (Math.random() < 0.5) {
+          this.type = "Homing Spinning";
+        }
+      }
+    }
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    // for smooth resizing of big enemy upon hit
+    this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
+
+  update() {
+    this.draw();
+
+    if (this.type === "Spinning") {
+      this.radians += 0.1;
+
+      this.center.x += this.velocity.x;
+      this.center.y += this.velocity.y;
+
+      this.x = this.center.x + Math.cos(this.radians) * 30;
+      this.y = this.center.y + Math.sin(this.radians) * 30;
+    } else if (this.type === "Homing") {
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+    } else if (this.type === "Homing Spinning") {
+      this.radians += 0.1;
+
+      const angle = Math.atan2(
+        player.y - this.center.y,
+        player.x - this.center.x
+      );
+      this.velocity.x = Math.cos(angle);
+      this.velocity.y = Math.sin(angle);
+
+      this.center.x += this.velocity.x;
+      this.center.y += this.velocity.y;
+
+      this.x = this.center.x + Math.cos(this.radians) * 30;
+      this.y = this.center.y + Math.sin(this.radians) * 30;
+    } else {
+      // linear movement
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+    }
+  }
+}
+
+const friction = 0.99;
+
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01;
+  }
+}
