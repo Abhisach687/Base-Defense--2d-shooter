f864961a4ebca0c4155ce7c3dd8e0643773ebcbb
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2023-12-10 11:22:44 +0545

    power ups

--- js/classes.js
+++ js/classes.js
@@ -180,3 +180,51 @@ class Particle {
     this.alpha -= 0.01;
   }
 }
+
+class PowerUp {
+  constructor({ position = { x: 0, y: 0 }, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.image = new Image();
+    this.image.src = "./img/lightningBolt.png";
+
+    this.alpha = 1;
+    this.direction = -1;
+
+    this.radians = 0;
+  }
+
+  animateAlpha() {
+    // Update alpha
+    this.alpha += this.direction * 0.01; // 0.01 is the speed of the fade effect
+
+    // Reverse direction at min/max alpha
+    if (this.alpha <= 0 || this.alpha >= 1) {
+      this.direction *= -1;
+    }
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.translate(
+      this.position.x + this.image.width / 2,
+      this.position.y + this.image.height / 2
+    );
+    c.rotate(this.radians);
+    c.translate(
+      -this.position.x - this.image.width / 2,
+      -this.position.y - this.image.height / 2
+    );
+    c.drawImage(this.image, this.position.x, this.position.y);
+    c.restore();
+  }
+
+  update() {
+    this.animateAlpha();
+    this.draw();
+    this.radians += 0.01;
+    this.position.x += this.velocity.x;
+  }
+}
